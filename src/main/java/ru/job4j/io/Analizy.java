994f06e0d762ab6ea33af6b9db456ab0e779d20c
VladislavCat <kshift84@gmail.com>
2022-05-07 19:36:06 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,17 +7,17 @@ import java.util.StringJoiner;
 public class Analizy {
 
     public void unavailable(String source, String target) {
-        String s = null;
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            String line = read.readLine();
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                String line = read.readLine();
             boolean flagCounter = false;
             while (line != null) {
                 String[] arrStr = line.split(" ");
                 if (("400".equals(arrStr[0]) || "500".equals(arrStr[0])) && !flagCounter) {
-                    s = s == null ? arrStr[1] + ";" : s + arrStr[1] + ";";
+                    out.print(arrStr[1] + ";");
                     flagCounter = true;
                 } else if ((flagCounter) && ("200".equals(arrStr[0]) || "300".equals(arrStr[0]))) {
-                    s += arrStr[1] + "\r\n";
+                    out.println(arrStr[1]);
                     flagCounter = false;
                     }
                 line = read.readLine();
@@ -25,8 +25,6 @@ public class Analizy {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            out.print(s);
         } catch (IOException e) {
             e.printStackTrace();
         }
