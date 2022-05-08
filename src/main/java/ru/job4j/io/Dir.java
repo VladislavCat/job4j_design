cfd0bd9b72f3c9f7219ed01bf6437fa7a9bfc495
VladislavCat <kshift84@gmail.com>
2022-05-08 19:27:11 +0300

    4.0. File [#252491]111

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            int sizeSubFile = volumeDirectory(subfile);
+            System.out.println(String.valueOf(subfile.getAbsoluteFile()) + " " + sizeSubFile);
+        }
+    }
+
+    private static int volumeDirectory(File file) {
+        int rsl = 0;
+        if (file.isDirectory()) {
+            for (File file1 : file.listFiles()) {
+                rsl += volumeDirectory(file1);
+            }
+        } else {
+            rsl += file.length();
+        }
+        return rsl;
+    }
+}
