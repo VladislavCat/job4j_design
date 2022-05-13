c3250ebaf2484636c45f62a4eb79cbc2b70ebbe9
VladislavCat <kshift84@gmail.com>
2022-05-14 00:43:08 +0300

    Дубликаты

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,21 +8,21 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Map<FileProperty, String> setOriginalFile = new HashMap<>();
-    Map<String, FileProperty> duplicatesPath = new HashMap<>();
+    Map<FileProperty, List<Path>> setOriginalFile = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty tmpFP = new FileProperty(file.toFile().length(), file.getFileName().toString());
         if (setOriginalFile.containsKey(tmpFP)) {
-            duplicatesPath.putIfAbsent(file.toFile().getAbsolutePath(), tmpFP);
-            duplicatesPath.put(setOriginalFile.get(tmpFP), tmpFP);
+            setOriginalFile.get(tmpFP).add(file);
+        } else {
+            setOriginalFile.put(tmpFP, new ArrayList<>());
+            setOriginalFile.get(tmpFP).add(file);
         }
-        setOriginalFile.put(tmpFP, file.toFile().getAbsolutePath());
         return super.visitFile(file, attrs);
     }
 
     public void getListDuplicates() {
-        duplicatesPath.keySet().forEach(System.out::println);
+        setOriginalFile.values().stream().filter(s -> s.size() > 1).forEach(s -> s.forEach(System.out::println));
     }
 }
