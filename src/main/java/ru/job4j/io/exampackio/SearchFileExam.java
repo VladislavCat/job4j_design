dc9c274d99d62f1bb8b90bcc6a1dc1ab903b9d36
VladislavCat <kshift84@gmail.com>
2022-05-19 19:27:55 +0300

    2. Поиск файлов по критерию [#783 #302702]

---
+++ src/main/java/ru/job4j/io/exampackio/SearchFileExam.java
@@ -0,0 +1,72 @@
+package ru.job4j.io.exampackio;
+
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class SearchFileExam {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Аргументы указаны неверно");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        checkArgs(argsName);
+        List<Path> listPath = Search.search(Path.of(argsName.get("d")),
+                createPredicateFromAllType(argsName.get("t"), argsName.get("n")));
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(argsName.get("o")))) {
+            for (Path path : listPath) {
+                out.write(path.toFile().getAbsolutePath().getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkArgs(ArgsName argsName) {
+        File file = new File(argsName.get("d"));
+        if (!file.isDirectory() || !file.exists()) {
+            throw new IllegalArgumentException("Указанный путь не является директорией или не существует");
+        }
+        String typeFind = argsName.get("t");
+        if (!"mask".equals(typeFind) && !"name".equals(typeFind) && !"regex".equals(typeFind)) {
+            throw new IllegalArgumentException("Неверно указан тип поиска элемента/ов");
+        }
+    }
+    /**
+    Создание фильтра типа поиска для маски данных
+     */
+    private static String createExtensionForMask(String s) {
+        String rsl = "";
+        char symbolString = s.charAt(s.length() - 1);
+        for (int i = 2; i < s.length(); i++) {
+            if (symbolString != '?' && symbolString != '*') {
+                rsl = symbolString + rsl;
+            }
+            symbolString = s.charAt(s.length() - i);
+        }
+        return rsl;
+    }
+
+    /**
+     * Создание условия для нахождения файла в зависимости от типа поиска
+     * @return Predicate<Path>
+     */
+    private static Predicate<Path> createPredicateFromAllType(String typeFind, String fileName) {
+        Predicate<Path> rsl = null;
+        if ("name".equals(typeFind)) {
+            rsl = s -> s.getFileName().toString().equals(fileName);
+        } else if ("mask".equals(typeFind)) {
+            String key = createExtensionForMask(fileName);
+            rsl = s -> s.getFileName().toString().endsWith(key);
+        } else if ("regex".equals(typeFind)) {
+            rsl = path -> Pattern.compile(fileName).matcher(path.getFileName().toString()).find();
+        }
+        return rsl;
+    }
+}
