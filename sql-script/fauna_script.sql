1327bfdd0d2f8d2368c3550b51b71e4a493e837e
VladislavCat <kshift84@gmail.com>
2022-06-01 01:02:34 +0300

    0. Select [#6861]

--- sql-script/fauna_script.sql
+++ sql-script/fauna_script.sql
@@ -1,4 +1,4 @@
-SELECT FROM fauna WHERE name LIKE '%fish%';
-SELECT FROM fauna WHERE avg_age > 10000 and avg_age < 21000;
-SELECT FROM fauna WHERE discovery_date is null;
-SELECT FROM fauna WHERE discovery_date < '1950-01-01';
+SELECT name FROM fauna WHERE name LIKE '%fish%';
+SELECT name FROM fauna WHERE avg_age > 10000 and avg_age < 21000;
+SELECT name FROM fauna WHERE discovery_date is null;
+SELECT name FROM fauna WHERE discovery_date < '1950-01-01';
